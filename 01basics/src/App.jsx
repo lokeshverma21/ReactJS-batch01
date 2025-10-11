2e1d45b392f832b93bb2cc29750a4c0b479bae62
lokeshverma21 <lokesh21980@gmail.com>
2025-10-11 19:11:58 +0530

    first commit

---
+++ 01basics/src/App.jsx
@@ -0,0 +1,55 @@
+// function App() {
+//   return (
+//     <>
+//       <h1>Hello</h1>
+//     </>
+//   )
+// }
+
+// export default App
+
+// function App(){
+//   return(
+//     <div>
+
+//     </div>
+//   )
+// }
+
+
+
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>hello world!</h1>
+      </div>
+      <div>
+        <h1>hello world!</h1>
+      </div>
+      <div>
+        <h1>hello world!</h1>
+      </div>
+      <div>
+        <h1>hello world!</h1>
+      </div>
+      <div>
+        <h1>hello world!</h1>
+      </div>
+      <div>
+        <h1>hello world!</h1>
+      </div>
+      <div>
+        <h1>hello world!</h1>
+      </div>
+      <div>
+        <h1>hello world!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
